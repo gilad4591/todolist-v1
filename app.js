722d691e6be8c9911fcda8da4a039d8e251663ee
Gilad Cohen <gilad4591@gmail.com>
2021-07-15 16:13:28 +0300

    added new features - remove list, reset list

--- app.js
+++ app.js
@@ -1,35 +1,192 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const date = require(__dirname + '/date.js');
+const mongoose = require('mongoose');
+let alert = require('alert');
+
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-let items = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.set('useFindAndModify', false);
+
+const itemsSchema = {
+  name: String,
+};
+var localFoundLists = [];
+
+const Item = mongoose.model("Item", itemsSchema);
+const item1 = new Item({
+  name: "Welcome to your todoList"
+});
+const item2 = new Item({
+  name: "Hit the + button to add new item"
+});
+const item3 = new Item({
+  name: "<-- Hit this to delete an item"
+});
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+}
+
+const List = mongoose.model("List", listSchema);
+
+const defaultItems = [item1, item2, item3];
+
 app.get('/', function(req, res) {
-  const day = date.getDate();
-  res.render("list", {
-    dayType: day,
-    newListItems: items
+  List.find(function(err, foundLists) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(foundLists);
+      localFoundLists = foundLists;
+    }
+  });
+  Item.find(function(err, foundItems) {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(error) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Successfully saved deafult items to DB");
+        }
+      });
+      res.redirect('/');
+    }
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("list", {
+        listTitle: "Default",
+        newListItems: foundItems,
+        lists: localFoundLists
+      });
+    }
   });
 });
 
 app.post('/', function(req, res) {
-  if (req.body.item) {
-    items.push(req.body.item);
-  } else if (req.body.remove) {
-    items.splice(req.body.remove, 1);
+  const itemName = req.body.item;
+  const listName = req.body.list;
+
+  const item = new Item({
+    name: itemName
+  });
+  if (listName === 'Today') {
+    item.save();
+    res.redirect('/');
+  } else {
+    List.findOne({
+      name: listName
+    }, function(err, foundList) {
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect('/' + listName);
+    });
   }
-  else if (req.body.reset){
-    items = [];
+});
+
+app.post('/reset', function(req, res) {
+  const listName = req.body.listName;
+  if (listName === "Today") {
+    Item.collection.drop();
+    res.redirect('/');
+  } else {
+    console.log(listName);
+    List.findOneAndDelete({
+      name: listName
+    }, function(err, foundList) {
+      if (!err) {
+        res.redirect('/' + listName);
+      }
+    });
   }
-  res.redirect('/');
+});
+
+app.post('/delete', function(req, res) {
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === 'Today') {
+    Item.findByIdAndDelete(checkedItemId, function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Removed succesfully");
+        res.redirect('/');
+      }
+    });
+  } else {
+    List.findOneAndUpdate({
+      name: listName
+    }, {
+      $pull: {
+        items: {
+          _id: checkedItemId
+        }
+      }
+    }, function(err, foundList) {
+      if (!err) {
+        res.redirect('/' + listName);
+      }
+    });
+  }
+});
+
+app.post('/remove', function(req,res){
+  const listName = req.body.remove;
+    List.findOneAndDelete({name: listName}, function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Removed succesfully");
+        res.redirect('/');
+      }
+    });
+});
+
+app.get('/:customListName', function(req, res) {
+  const customListName = req.params.customListName[0].toUpperCase() + req.params.customListName.slice(1).toLowerCase();
+  List.findOne({
+    name: customListName
+  }, function(err, foundList) {
+    if (!err) {
+      if (!foundList) {
+        if (customListName!= "Default"){
+        // localFoundLists.add(customListName);
+        List.create({
+          name: customListName,
+          items: defaultItems
+        }, function(err) {
+          if (!err) {
+            res.redirect("/" + customListName);
+
+          }
+        });
+      }
+      else {
+        res.redirect("/");
+      }
+      } else {
+        res.render("list", {
+          listTitle: foundList.name,
+          newListItems: foundList.items,
+          lists: localFoundLists
+        });
+      }
+    }
+  });
 });
 
 app.get('/about', function(req, res) {
